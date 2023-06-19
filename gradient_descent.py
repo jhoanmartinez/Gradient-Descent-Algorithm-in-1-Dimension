f2889b16f80a929a07731807fe7bdc33ae4334a0
jhoanmartinez <jhoao1320@gmail.com>
2023-06-18 21:08:59 -0500

    set up

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,14 +1,13 @@
 """
 Algorithm:
-    1-Pick a random value from math function values
+    1-Pick a random value from cost function values
     2-Process and get it derivate
-    3-Process descent random_fx_value - grad_value * learning_rate
-    4-Update the random value = descent value from step 3
+    3-Process descent random_fx_value -= grad_value * learning_rate
 """
 
 import random
 
-def fx(x):
+def fx(x: float) -> float:
     """
     Computes the value of the math function.
 
@@ -18,9 +17,9 @@ def fx(x):
     Returns:
         float: The result of the math function.
     """
-    return 3 * x ** 2 - 3 * x + 4
+    return x ** 2 
 
-def deriv(x):
+def deriv(x: float) -> float:
     """
     Computes the derivative of the math function.
 
@@ -30,22 +29,24 @@ def deriv(x):
     Returns:
         float: The derivative value.
     """
-    return 6 * x - 3
-
+    return 2 * x
+    
 # Set learning rate parameter
 learning_rate = 0.01
+
+# Threshold to finish trainig once the difference between currnet iteration and last one is equals or lower than
+threshold = 0.0001
 
 # Number of training epochs
 training_epochs = 100
 
-# Generate a list of function values
+# Generate a list of values from 0 to 100 with the fx(x) function
 fx_list = [fx(i) for i in range(100)]
 
 # Choose a random value from the function values
 random_x = random.choice(fx_list)
-random_x_index = fx_list.index(random_x)
 
-print(f"Random x value taken = {random_x}, index = {random_x_index}\n")
+print(f"Random x value taken = {random_x}\n")
 
 # Gradient descent algorithm
 for epoch in range(training_epochs):
@@ -55,7 +56,4 @@ for epoch in range(training_epochs):
     # Update the value of random_x using the gradient descent update rule
     random_x -= grad * learning_rate
 
-    # Print the updated value and gradient
-    print(f"Epoch {epoch+1}: random_x = {random_x}, gradient = {grad}\n")
-
-print("local min =", random_x)
+print("local min in cost function =", random_x)
